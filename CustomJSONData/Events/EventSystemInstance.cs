59e44a12548a4c5b811619c0407032269c6e8c4b
Arti Kearney <artemis.wk@gmail.com>
2019-02-24 03:28:20 -0800

    Add tree utilities and beginnings of the event system

---
+++ CustomJSONData/Events/EventSystemInstance.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CustomJSONData.Trees;
+
+namespace CustomJSONData.Events
+{
+    public class EventSystemInstance
+    {
+        public readonly string id;
+        EventSystemInstance(string id)
+        {
+            this.id = id;
+        }
+        public static EventSystemInstance create(string id)
+        {
+            if (id == null) throw new Exception("id must not be null");
+            return new EventSystemInstance(id);
+        }
+
+        public delegate dynamic MinimalHandler(dynamic args);
+        public delegate dynamic AheadHandler(dynamic args, float aheadTime);
+        public delegate dynamic ManualNextHandler(dynamic args, MinimalHandler next);
+        public delegate dynamic FullHandler(dynamic args, MinimalHandler next, float aheadTime);
+
+        public FullHandler toFullHandler(MinimalHandler h) => (args, next, aheadTime) => mergeTrees(h(args), next(args), false);
+        public FullHandler toFullHandler(AheadHandler h) => (a, n, t) => mergeTrees(h(a, t), n(a));
+        public FullHandler toFullHandler(ManualNextHandler h) => (a, n, t) => h(a, n);
+
+        /// <summary>
+        /// TODO: document handler registration
+        /// </summary>
+        /// <param name="handler"></param>
+        /// <param name="priority"></param>
+        /// <param name="aheadTime"></param>
+        public void register(FullHandler handler, Priority priority = null, float aheadTime = 0)
+        {
+            if (priority == null) priority = Priority.DEFAULT;
+            //TODO: implement handler registration
+        }
+
+        public void register(MinimalHandler handler, Priority priority = null, float aheadTime = 0) => register(toFullHandler(handler), priority, aheadTime);
+        public void register(AheadHandler handler, Priority priority = null, float aheadTime = 0) => register(toFullHandler(handler), priority, aheadTime);
+        public void register(ManualNextHandler handler, Priority priority = null, float aheadTime = 0) => register(toFullHandler(handler), priority, aheadTime);
+
+        /// <summary>
+        /// TODO: document event invocation
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <param name="args"></param>
+        /// <param name="originInfo"></param>
+        /// <returns></returns>
+        public dynamic invoke(string eventName, dynamic args, dynamic originInfo)
+        {
+            // TODO: event invocation
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomJSONData/Events/Priority.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomJSONData.Events
+{
+    public class Priority
+    {
+        /// <summary>
+        /// A Priority of a lower sandboxingLevel n is always considered higher than a Priority of a sandboxingLevel > n, regardless of other fields.
+        /// </summary>
+        public readonly int sandboxingLevel = 0;
+
+        /// <summary>
+        /// The default sorting metric for priorities. 
+        /// </summary>
+        public readonly int numeric = 0;
+
+        /// <summary>
+        /// Overrides numeric priority, guaranteeing the handler with this Priority will run before handlers of the same sandboxingLevel registered through EventSystemInstances with ids in this collection, except where this would create cycles. To break cycles, constraints are removed in ascending order of priority.
+        /// </summary>
+        public readonly ReadOnlyCollection<string> before;
+
+        /// <summary>
+        /// Overrides numeric priority, guaranteeing the handler with this Priority will run after handlers of the same sandboxingLevel registered through EventSystemInstances with ids in this collection, except where this would create cycles. To break cycles, constraints are removed in ascending order of priority.
+        /// </summary>
+        public readonly ReadOnlyCollection<string> after;
+
+        public Priority(int numeric, List<string> before = null, List<string> after = null, int sandboxingLevel = 0)
+        {
+            this.sandboxingLevel = sandboxingLevel;
+            this.numeric = numeric;
+            this.before = before?.AsReadOnly();
+            this.after = after?.AsReadOnly();
+        }
+
+        public static readonly Priority DEFAULT = new Priority(0);
+        public static readonly Priority SANDBOXED = new Priority(0, null, null, 1);
+    }
+}
