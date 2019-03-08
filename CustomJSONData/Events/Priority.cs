e30312a59ce802615bcd08dc3fd780da310c18df
Arti Kearney <artemis.wk@gmail.com>
2019-03-07 19:05:48 -0800

    Remove sandboxingLevel; add casts between int and priority

--- CustomJSONData/Events/Priority.cs
+++ CustomJSONData/Events/Priority.cs
@@ -10,34 +10,36 @@ namespace CustomJSONData.Events
     public class Priority
     {
         /// <summary>
-        /// A Priority of a lower sandboxingLevel n is always considered higher than a Priority of a sandboxingLevel > n, regardless of other fields.
-        /// </summary>
-        public readonly int sandboxingLevel = 0;
-
-        /// <summary>
         /// The default sorting metric for priorities. 
         /// </summary>
         public readonly int numeric = 0;
 
         /// <summary>
-        /// Overrides numeric priority, guaranteeing the handler with this Priority will run before handlers of the same sandboxingLevel registered through EventSystemInstances with ids in this collection, except where this would create cycles. To break cycles, constraints are removed in ascending order of priority. Can be null.
+        /// Overrides numeric priority, guaranteeing the handler with this Priority will run before handlers registered through EventSystemInstances with ids in this collection, except where this would create cycles. To break cycles, constraints are removed in ascending order of priority. Can be null.
         /// </summary>
         public readonly ReadOnlyCollection<string> before;
 
         /// <summary>
-        /// Overrides numeric priority, guaranteeing the handler with this Priority will run after handlers of the same sandboxingLevel registered through EventSystemInstances with ids in this collection, except where this would create cycles. To break cycles, constraints are removed in ascending order of priority. Can be null.
+        /// Overrides numeric priority, guaranteeing the handler with this Priority will run after handlers registered through EventSystemInstances with ids in this collection, except where this would create cycles. To break cycles, constraints are removed in ascending order of priority. Can be null.
         /// </summary>
         public readonly ReadOnlyCollection<string> after;
 
-        public Priority(int numeric, List<string> before = null, List<string> after = null, int sandboxingLevel = 0)
+        public Priority(int numeric, List<string> before = null, List<string> after = null)
         {
-            this.sandboxingLevel = sandboxingLevel;
             this.numeric = numeric;
             this.before = before?.AsReadOnly();
             this.after = after?.AsReadOnly();
         }
 
         public static readonly Priority DEFAULT = new Priority(0);
-        public static readonly Priority SANDBOXED = new Priority(0, null, null, 1);
+        public static readonly Priority HIGH = new Priority(1000);
+        public static readonly Priority LOW = new Priority(-1000);
+
+        public static implicit operator Priority(int p) => new Priority(p);
+
+        /// <summary>
+        /// While it's fine to explicitly convert an int to a priority, converting back is explicit because it can lose information
+        /// </summary>
+        public static explicit operator int(Priority p) => p.numeric;
     }
 }
