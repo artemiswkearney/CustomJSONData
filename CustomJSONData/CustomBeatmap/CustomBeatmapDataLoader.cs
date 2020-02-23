55129628c22902efcaf7108020b90c510f74f728
Aeroluna <aeroluna5@gmail.com>
2020-02-23 12:35:06 -0700

    Use base game methods instead of own

--- CustomJSONData/CustomBeatmap/CustomBeatmapDataLoader.cs
+++ CustomJSONData/CustomBeatmap/CustomBeatmapDataLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -10,25 +11,6 @@ namespace CustomJSONData.CustomBeatmap
 {
     public class CustomBeatmapDataLoader
     {
-        // Token: 0x0600125E RID: 4702 RVA: 0x00040454 File Offset: 0x0003E654
-        private static float GetRealTimeFromBPMTime(float bmpTime, float beatsPerMinute, float shuffle, float shufflePeriod)
-        {
-            float num = bmpTime;
-            if (shufflePeriod > 0f)
-            {
-                bool flag = (int)(num * (1f / shufflePeriod)) % 2 == 1;
-                if (flag)
-                {
-                    num += shuffle * shufflePeriod;
-                }
-            }
-            if (beatsPerMinute > 0f)
-            {
-                num = num / beatsPerMinute * 60f;
-            }
-            return num;
-        }
-
         // Token: 0x0600125F RID: 4703 RVA: 0x000404A0 File Offset: 0x0003E6A0
         public static CustomBeatmapData GetBeatmapDataFromBeatmapSaveData(List<CustomBeatmapSaveData.NoteData> notesSaveData, List<CustomBeatmapSaveData.ObstacleData> obstaclesSaveData, 
                                                                           List<CustomBeatmapSaveData.EventData> eventsSaveData, float beatsPerMinute, float shuffle, float shufflePeriod, 
@@ -36,6 +18,10 @@ namespace CustomJSONData.CustomBeatmap
         {
             try
             {
+                // why is IPA's reflectionutil missing InvokeMethod :(
+                MethodInfo GetRealTimeFromBPMTime = typeof(BeatmapDataLoader).GetMethod("GetRealTimeFromBPMTime", BindingFlags.Static | BindingFlags.NonPublic);
+                MethodInfo ProcessBasicNotesInTimeRow = typeof(BeatmapDataLoader).GetMethod("ProcessBasicNotesInTimeRow", BindingFlags.Static | BindingFlags.NonPublic);
+
                 // remainder of this method copied from SongLoaderPlugin@12de0cf, with changes:
                 // BeatmapData, NoteData, EventData, and ObstacleData replaced with their Custom counterparts
                 // customData fields propagated to new objects
@@ -52,7 +38,7 @@ namespace CustomJSONData.CustomBeatmap
                 float num3 = 0f;
                 foreach (CustomBeatmapSaveData.NoteData noteData2 in notesSaveData)
                 {
-                    float realTimeFromBPMTime = GetRealTimeFromBPMTime(noteData2.time, beatsPerMinute, shuffle, shufflePeriod);
+                    float realTimeFromBPMTime = Convert.ToSingle(GetRealTimeFromBPMTime.Invoke(null, new object[] { noteData2.time, beatsPerMinute, shuffle, shufflePeriod }));
                     if (num3 > realTimeFromBPMTime)
                     {
                         Debug.LogError("Notes are not ordered.");
@@ -76,7 +62,7 @@ namespace CustomJSONData.CustomBeatmap
                     NoteCutDirection cutDirection = noteData2.cutDirection;
                     if (list2.Count > 0 && list2[0].time < realTimeFromBPMTime - 0.001f && type.IsBasicNote())
                     {
-                        ProcessBasicNotesInTimeRow(list2, realTimeFromBPMTime);
+                        ProcessBasicNotesInTimeRow.Invoke(null, new object[] { list2.Cast<NoteData>().ToList(), realTimeFromBPMTime });
                         num2 = list2[0].time;
                         list2.Clear();
                     }
@@ -93,13 +79,13 @@ namespace CustomJSONData.CustomBeatmap
                         list2.Add(noteData);
                     }
                 }
-                ProcessBasicNotesInTimeRow(list2, float.MaxValue);
+                ProcessBasicNotesInTimeRow.Invoke(null, new object[] { list2.Cast<NoteData>().ToList(), float.MaxValue });
                 foreach (CustomBeatmapSaveData.ObstacleData obstacleData in obstaclesSaveData)
                 {
-                    float realTimeFromBPMTime2 = GetRealTimeFromBPMTime(obstacleData.time, beatsPerMinute, shuffle, shufflePeriod);
+                    float realTimeFromBPMTime2 = Convert.ToSingle(GetRealTimeFromBPMTime.Invoke(null, new object[] { obstacleData.time, beatsPerMinute, shuffle, shufflePeriod }));
                     int lineIndex2 = obstacleData.lineIndex;
                     ObstacleType type2 = obstacleData.type;
-                    float realTimeFromBPMTime3 = GetRealTimeFromBPMTime(obstacleData.duration, beatsPerMinute, shuffle, shufflePeriod);
+                    float realTimeFromBPMTime3 = Convert.ToSingle(GetRealTimeFromBPMTime.Invoke(null, new object[] { obstacleData.duration, beatsPerMinute, shuffle, shufflePeriod }));
                     int width = obstacleData.width;
                     CustomObstacleData item = new CustomObstacleData(num++, realTimeFromBPMTime2, lineIndex2, type2, realTimeFromBPMTime3, width, obstacleData._customData ?? Tree());
                     int number2 = lineIndex2;
@@ -111,7 +97,7 @@ namespace CustomJSONData.CustomBeatmap
                 }
                 foreach (CustomBeatmapSaveData.EventData eventData in eventsSaveData)
                 {
-                    float realTimeFromBPMTime4 = GetRealTimeFromBPMTime(eventData.time, beatsPerMinute, shuffle, shufflePeriod);
+                    float realTimeFromBPMTime4 = Convert.ToSingle(GetRealTimeFromBPMTime.Invoke(null, new object[] { eventData.time, beatsPerMinute, shuffle, shufflePeriod }));
                     BeatmapEventType type3 = eventData.type;
                     int value = eventData.value;
                     CustomBeatmapEventData item2 = new CustomBeatmapEventData(realTimeFromBPMTime4, type3, value, eventData.customData ?? Tree());
@@ -126,7 +112,7 @@ namespace CustomJSONData.CustomBeatmap
                 foreach (CustomBeatmapSaveData.CustomEventData customEventData in customEventsSaveData)
                 {
                     if (!customEvents.ContainsKey(customEventData.type)) customEvents[customEventData.type] = new List<CustomEventData>();
-                    customEvents[customEventData.type].Add(new CustomEventData(GetRealTimeFromBPMTime(customEventData.time, beatsPerMinute, shuffle, shufflePeriod), customEventData.type, customEventData.data ?? Tree()));
+                    customEvents[customEventData.type].Add(new CustomEventData(Convert.ToInt32(GetRealTimeFromBPMTime.Invoke(null, new object[] { customEventData.time, beatsPerMinute, shuffle, shufflePeriod })), customEventData.type, customEventData.data ?? Tree()));
                 }
                 foreach (var pair in customEvents)
                 {
@@ -154,25 +140,6 @@ namespace CustomJSONData.CustomBeatmap
                 Debug.LogError("Exception loading CustomBeatmap!");
                 Debug.LogError(e);
                 throw e;
-            }
-        }
-
-        // Token: 0x06001260 RID: 4704 RVA: 0x00040864 File Offset: 0x0003EA64
-        private static void ProcessBasicNotesInTimeRow(List<CustomNoteData> notes, float nextRowTime)
-        {
-            if (notes.Count == 2)
-            {
-                CustomNoteData noteData = notes[0];
-                CustomNoteData noteData2 = notes[1];
-                if (noteData.noteType != noteData2.noteType && ((noteData.noteType == NoteType.NoteA && noteData.lineIndex > noteData2.lineIndex) || (noteData.noteType == NoteType.NoteB && noteData.lineIndex < noteData2.lineIndex)))
-                {
-                    noteData.SetNoteFlipToNote(noteData2);
-                    noteData2.SetNoteFlipToNote(noteData);
-                }
-            }
-            for (int i = 0; i < notes.Count; i++)
-            {
-                notes[i].timeToNextBasicNote = nextRowTime - notes[i].time;
             }
         }
     }
